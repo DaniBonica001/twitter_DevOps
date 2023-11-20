1a4888b0e5810ba8d06091158551088fcc3efec7
DaniBonica001 <danielabc_0106@hotmail.com>
2023-11-19 22:09:30 -0500

    Added: Mongo db in a pod

--- terraform/modules/cosmosdb/output.tf
+++
@@ -1,3 +0,0 @@
-output "mongodb_connection_string"{
-    value=azurerm_cosmosdb_account.az-cosmos.connection_strings.0   
-}
--- terraform/modules/cosmosdb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_location" {
-  type        = string
-  description = "The location/region where the resource group will be created."
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group in which to create the AKS cluster."
-}
